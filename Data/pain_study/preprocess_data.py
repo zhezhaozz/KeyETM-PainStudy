bd1831726469e6f430811a5428dbf16360babb36
Zhe Zhao <zzhaozhe@umich.edu>
2024-08-20 16:10:46 -0400

    Correct number of documents

--- Data/pain_study/preprocess_data.py
+++ Data/pain_study/preprocess_data.py
@@ -18,6 +18,8 @@ studies = studies.rename(columns={'Project.Title': 'title',
 studies['title'] = studies['title'].astype('string')
 studies['relevance'] = studies['relevance'].astype('string')
 studies['abstract'] = studies['abstract'].astype('string')
+studies['pain_confirmed'] = studies['pain_confirmed'].astype('string')
+studies['HEAL.Pain.'] = studies['HEAL.Pain.'].astype('string')
 #studies['HEAL.Pain.'] = studies['HEAL.Pain.'].astype('string')
 studies.info()
 
@@ -33,7 +35,7 @@ text_cols = ['title', 'relevance', 'abstract']
 studies['text_cleaned'] = studies[f'{text_cols[0]}'] + ' ' + studies[f'{text_cols[1]}'] + ' ' + studies[f'{text_cols[2]}']
 
 # only consider heal pain studies
-cleaned_dt = studies[(studies['pain_confirmed'] == 'Yes') | (studies['HEAL.Pain.'] == 'TRUE')]
+cleaned_dt = studies[(studies['pain_confirmed'] == 'Yes') | (studies['HEAL.Pain.'] == 'True')]
 
 # select relevant columns
 cleaned_dt = cleaned_dt[['ID', 'text_cleaned', 'primary_label','secondary_lebel','tertiary_label']]
@@ -43,12 +45,13 @@ pattern = r'[\r|\n|\t]'
 cleaned_dt['text_cleaned'] = cleaned_dt['text_cleaned'].str.replace(pattern, ' ', regex=True)
 
 # separate training set and testing set
-train_dt = cleaned_dt[cleaned_dt['primary_label'].isnull()]
-test_dt = cleaned_dt[cleaned_dt['primary_label'].notnull()]
+train_dt = cleaned_dt[cleaned_dt['primary_label'].isnull()] #648
+test_dt = cleaned_dt[cleaned_dt['primary_label'].notnull()] #47
 
 print(f"Number of rows for training set: {train_dt.shape[0]}")
 print(f"Number of rows for testing set: {test_dt.shape[0]}")
 
 # save data
+cleaned_dt.to_csv('pain_preprocessed_data.csv')
 train_dt.to_csv('pain_train_data.csv')
 test_dt.to_csv('pain_test_data.csv')
