272f3e73fcdb8585d5090b3e48c711bc37117650
Zhe Zhao <zzhaozhe@umich.edu>
2024-09-17 21:00:55 -0400

    cyclical scheduler

---
+++ embedded_topic_model/utils/scheduler.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def frange_cycle_linear(start, stop, n_epoch, n_cycle=4, ratio=0.5):
+    L = np.ones(n_epoch)
+    period = n_epoch/n_cycle
+    step = (stop-start)/(period*ratio) # linear schedule
+
+    for c in range(n_cycle):
+
+        v , i = start , 0
+        while v <= stop and (int(i+c*period) < n_epoch):
+            L[int(i+c*period)] = v
+            v += step
+            i += 1
+    return L    
